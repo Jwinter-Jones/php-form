6eb3a576a872f908cf8d75b1ce2400f026a9ab19
James Winter-Jones <50118609+Jwinter-Jones@users.noreply.github.com>
2020-11-19 11:36:32 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-echo '<p>This is PHP!</p>';
+//echo '<p>This is PHP!</p>';
+
+if(isset($_GET[])){//show feedback
+  
+}else{//show form
+  echo '
+  <form action="">
+  First Name <input type="text" name="First Name" />
+  <br />
+  <input type="submit">
+  
+ </form> 
+  ';
+}
+
+
+
 ?>
-<p>This is HTML!</p>
+
